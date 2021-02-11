cabf2666d098972cbac29cc7d6b0af67e78964e6
Artem <78876748+artemspb@users.noreply.github.com>
2021-02-11 22:34:11 +0300

    fic merge conflicts

--- user/index.js
+++ user/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const phrases = require('./ru')
 // exports
 class User {
@@ -11,17 +10,3 @@ class User {
   }
 }
 module.exports = User
-=======
-const phrases = require('./ru')
-// exports
-class User {
-  constructor (name) {
-    this.name = name
-  }
-
-  hi () {
-    console.log(`${phrases.hello} ${this.name}`)
-  }
-}
-module.exports = User
->>>>>>> 19bc058145cb2f3af2244c8fb11b4c62c8456279
