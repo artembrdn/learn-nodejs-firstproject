6f4318dac116f25b55fc93ee8ff6c0dd33c3b947
Artem <78876748+artemspb@users.noreply.github.com>
2021-02-23 21:02:09 +0300

    add static files send in public folder

--- file.js
+++ file.js
@@ -1,23 +1,63 @@
 const path = require('path')
 const fs = require('fs')
+const mime = require('mime')
+const PUBLIC_DIR = path.join(__dirname, 'public')
 
 exports.send = function (filePath, res) {
-  const fileName = path.basename(filePath)
-  const file = new fs.ReadStream('./files/' + fileName)
-  console.log('./files/' + fileName)
+  let fileName
+  try {
+    fileName = decodeURIComponent(path.basename(filePath))
+  } catch (error) {
+    res.statusCode = 400
+    res.setHeader('Content-Type', ' text/html; charset=utf-8')
+    res.end('Bad request')
+  }
+  const pathDir = path.dirname(filePath)
+  let fileLocation
+  let attachment = false
+  if (pathDir.indexOf('/download') === 0) {
+    fileLocation = `./files/${fileName}`
+    attachment = true
+  } else {
+    fileLocation = path.normalize(path.join(PUBLIC_DIR, filePath))
+    if (fileLocation.indexOf(PUBLIC_DIR) !== 0) {
+      res.statusCode = 400
+      res.setHeader('Content-Type', ' text/html; charset=utf-8')
+      res.end('Bad request')
+    }
+  }
+
+  try {
+    sendFile(fileLocation, fileName, attachment, res)
+  } catch (error) {
+    console.log(error)
+    res.statusCode = 400
+    res.setHeader('Content-Type', ' text/html; charset=utf-8')
+    res.end('Bad request')
+  }
+}
+
+function sendFile (filePath, fileName, attachment, res) {
+  const file = new fs.ReadStream(filePath)
 
   file.pipe(res)
   file.on('open', () => {
     console.log('open')
+    const fileMime = mime.getType(filePath)
     res.writeHead(200, {
-      'Content-Type': 'application/octet-stream',
-      'Content-Disposition': 'attachment; filename=' + fileName
+      'Content-Type': fileMime + '; charset=utf-8'
     })
+    if (attachment) {
+      res.writeHead(200, {
+        'Content-Disposition': 'attachment; filename=' + fileName
+      })
+    }
   })
   file.on('error', (err) => {
     if (err.code === 'ENOENT') {
       res.statusCode = 404
-      res.end('File not found')
+      res.setHeader('Content-Type', ' text/html; charset=utf-8')
+      res.end(`File ${fileName} not found`)
       console.log(err.code)
     } else {
       res.statusCode = 500
@@ -25,8 +65,15 @@ exports.send = function (filePath, res) {
       console.log(err.code)
     }
   })
+  file.on('close', () => {
+    console.log('file close')
+  })
   res.on('close', () => {
     console.log('res close')
+    file.destroy()
+  })
+  res.on('error', () => {
+    console.log('res error')
     file.destroy()
   })
 }
--- server.js
+++ server.js
@@ -7,20 +7,16 @@ const file = require('./file')
 function run () {
   const server = new http.Server()
   server.listen(8888, '127.0.0.1')
-
   server.on('request', (req, res) => {
     const urlParsed = new url.URL(req.url, 'http://' + req.headers.host + '/')
-
+    console.log(urlParsed.pathname)
     if (urlParsed.pathname === '/echo') {
       res.setHeader('Cache-control', 'no-cache')
       res.setHeader('Content-Type', ' text/html; charset=utf-8')
       res.statusCode = 200
       res.end(urlParsed.searchParams.get('message'))
-    } else if (urlParsed.pathname.indexOf('/download/') === 0) {
-      file.send(urlParsed.pathname, res)
     } else {
-      res.statusCode = 404
-      res.end('Страница не найдена')
+      file.send(urlParsed.pathname, res)
     }
   })
 }
